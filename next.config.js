cfb79ea054234e36812878590a72f8bc2acb0e54
ymcheung <ymcheung.tw@me.com>
2021-06-06 17:43:26 +0800

    Add back sitemap

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  i18n
+  i18n,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/sitemap');
+    }
+
+    return config;
+  }
 }
