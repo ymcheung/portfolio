f0eb770457dc81f24c1c878f6364b295f59f14f1
ymcheung <ymcheung.tw@me.com>
2021-06-16 12:07:30 +0800

    Move google fonts into _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,8 +97,6 @@ export default function Home() {
       <Head>
         <title>{TITLE}</title>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(webPageSchema) }} />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,300;0,400;0,600;0,800;1,400;1,700;1,800&display=swap" rel="stylesheet" />
       </Head>
       <HeadMeta />
       <Header responsive={{ '@initial': 'mobile', '@m768': 'tablet' }}>
--- pages/pie-clockin.tsx
+++ pages/pie-clockin.tsx
@@ -50,8 +50,6 @@ export default function ProjectPie() {
     <>
       <Head>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;500&family=Overpass:ital,wght@0,300;0,400;0,600;0,800;1,400;1,700;1,800&display=swap" rel="stylesheet" />
       </Head>
       <HeadMeta
         title={pageInfo.name}
