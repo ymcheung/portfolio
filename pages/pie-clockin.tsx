8f254cfcd17a05b821a5fc02880c85795c384b93
ymcheung <ymcheung.tw@me.com>
2021-07-21 16:11:38 +0800

    Update screenshots

--- pages/pie-clockin.tsx
+++ pages/pie-clockin.tsx
@@ -28,7 +28,7 @@ export default function ProjectPie() {
     name: 'Redesign UI of a Clock-In App',
     description: 'A side project exploring fluent UI design.',
     datePublished: '2020-08-29',
-    dateModified: '2021-07-09'
+    dateModified: '2021-07-22'
   };
 
   const router = useRouter();
