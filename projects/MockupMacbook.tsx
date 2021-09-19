791fee9af964e26c30e64154bdc47265d13fe501
ymcheung <ymcheung.tw@me.com>
2021-09-19 11:23:30 +0800

    Add Cover for Process

--- projects/MockupMacbook.tsx
+++ projects/MockupMacbook.tsx
@@ -22,7 +22,7 @@ const Part = styled('path', {
     screen: {
       duo: {
         fill: 'hsl($shade1500)',
-        stroke: 'hsl($shade1200)',
+        stroke: 'hsl($shade1400)',
         strokeWidth: '4px'
       },
       mono: {
@@ -60,7 +60,7 @@ const Part = styled('path', {
 
 const Image = styled('image', {
   width: '800px',
-  height: '425px'
+  height: '432px'
 });
 
 interface MockupProps {
@@ -73,11 +73,11 @@ export default function MockupMacbook({ image, theme, description } : MockupProp
   return(
     <Mockup viewBox="0 0 1000 546" responsive={{ '@m768': 'tablet', '@m1200': 'desktop' }} xmlns="http://www.w3.org/2000/svg">
       <title>{description}</title>
-      <Part screen={theme} d="m78 8.75759v510.48441c0 3.737 3.0275 6.758 6.7677 6.758h838.4643c3.741 0 6.768-3.023 6.768-6.758v-510.48441c0-3.73685-3.028-6.75759-6.768-6.75759h-838.4643c-3.7409 0-6.7677 3.02302-6.7677 6.75759z"/>
+      <Part screen={theme} d="M78 519.242V34C78 22.799 78 17.1984 80.1799 12.9202C82.0973 9.15695 85.1569 6.09734 88.9202 4.17987C93.1984 2 98.7989 2 110 2H898C909.201 2 914.802 2 919.08 4.17987C922.843 6.09734 925.903 9.15695 927.82 12.9202C930 17.1984 930 22.799 930 34V519.242C930 522.977 926.973 526 923.232 526H84.7677C81.0275 526 78 522.979 78 519.242Z"/>
       <Part keyboardside={theme} d="m4 534v-14c0-2.523 1.50908-4 4-4h984c2.488 0 4 1.47 4 4v14c0 12 0 12-32 12h-928c-32 0-32 0-32-12z"/>
       <Part concave={theme} d="m426 516h148c0 14-17.077 14-18.215 14-33.187 0-67.238 0-111.57 0-1.138 0-18.215 0-18.215-14z"/>
       <Part browsertab={theme} d="m100 42h32c8.176 0 8.099 0 8.017-11.9972-.016-2.2091 1.774-4.0028 3.983-4.0028h68c2.209 0 3.999 1.7937 3.983 4.0028-.082 11.9972-.159 11.9972 8.017 11.9972h676v18h-800z"/>
-      <Image href={image} x="100" y="60"/>
+      <Image href={image} x="100" y="60" preserveAspectRatio="xMidYMin meet"/>
     </Mockup>
   );
 }
---
+++ projects/process/Cover.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import { styled } from 'stitches.config';
+import { screenMobile, screenTablet } from '@utils/screens';
+
+import { useTheme } from 'next-themes';
+
+import { Wrap, Device } from '@projects/cover';
+import MockupMacbook from '@projects/MockupMacbook';
+
+const Screenshot = styled(Image, {
+  width: '100%',
+  position: 'relative',
+  zIndex: 1
+});
+
+export default function Cover() {
+  const { resolvedTheme } = useTheme();
+  let isDarkTheme = resolvedTheme === 'dark';
+
+  return(
+    <Wrap>
+      <Device responsive={{ '@initial': 'mobile', '@m768': 'tablet' }} model={{ '@m768': 'tabletMacbook' }}>
+        {screenMobile && isDarkTheme &&
+          <Screenshot src="/projects/process/coverMobileDark.webp" width={272} height={484} alt="Mobile Landing Page of Design Process for Pros Translation" />
+        }
+        {screenMobile && !isDarkTheme &&
+          <Screenshot src="/projects/process/coverMobileLight.webp" width={272} height={484} alt="Mobile Landing Page of Design Process for Pros Translation" />
+        }
+        {screenTablet &&
+          <MockupMacbook image="/projects/process/coverDesktop.jpg" theme="duo" description="Desktop Landing Page of Design Process for Pros Translation" />
+        }
+      </Device>
+    </Wrap>
+  )
+}
