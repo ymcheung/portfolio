61bc746144b93b0d4fcc2286ef7845a30ac8e880
ymcheung <ymcheung.tw@me.com>
2022-03-02 12:21:24 +0800

    Add dark/light screens for Carrier Express

--- projects/carrier/Cover.tsx
+++ projects/carrier/Cover.tsx
@@ -1,10 +1,13 @@
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 import { styled } from 'stitches.config';
 
 import { Device } from '@projects/cover';
-import homeIndexEn from '@public/projects/carrier/gallery/en/homeIndex.webp';
-import homeIndexTw from '@public/projects/carrier/gallery/tw/homeIndex.webp';
+import homeIndexEnDark from '@public/projects/carrier/gallery/en/homeIndexDark.webp';
+import homeIndexTwDark from '@public/projects/carrier/gallery/tw/homeIndexDark.webp';
+import homeIndexEnLight from '@public/projects/carrier/gallery/en/homeIndexLight.webp';
+import homeIndexTwLight from '@public/projects/carrier/gallery/tw/homeIndexLight.webp';
 
 const ScreenshotMobile = styled(Image, {
   position: 'relative',
@@ -34,18 +37,26 @@ const ScreenshotTablet = styled('img', {
 
 export default function Cover() {
   const router = useRouter();
+  const { resolvedTheme } = useTheme();
 
-  const homeIndex = router.locale === 'en' ? homeIndexEn : homeIndexTw;
+  const homeIndexLight = router.locale === 'en' ? homeIndexEnLight : homeIndexTwLight;
+  const homeIndexDark = router.locale === 'en' ? homeIndexEnDark : homeIndexTwDark;
   const imgLocale = router.locale === 'en' ? 'en' : 'tw';
+  const imgAffix = resolvedTheme === 'dark' ? 'Dark' : 'Light';
 
   return(
     <>
       <Device display={{ '@m768': 'none' }} responsive="mobile">
-        <ScreenshotMobile src={homeIndex} layout="responsive" quality={100} alt="Screenshot: Upcoming Health Checks" />
+        {resolvedTheme === 'dark' &&
+          <ScreenshotMobile src={homeIndexDark} layout="responsive" quality={100} alt="Screenshot: Upcoming Health Checks" />
+        }
+        {resolvedTheme === 'light' &&
+          <ScreenshotMobile src={homeIndexLight} layout="responsive" quality={100} alt="Screenshot: Upcoming Health Checks" />
+        }
       </Device>
       <Device display={{ '@initial': 'none', '@m768': 'block' }} responsive="tablet" model="mobileDual">
-        <ScreenshotTablet src={`/projects/carrier/gallery/${imgLocale}/homeIndex.jpg`} placement="left" alt="Screenshot: Upcoming Health Checks" loading="lazy" />
-        <ScreenshotTablet src={`/projects/carrier/gallery/${imgLocale}/furryShowOnTime.jpg`} placement="right" alt="Screenshot: View the details of Seven" loading="lazy" />
+        <ScreenshotTablet src={`/projects/carrier/gallery/${imgLocale}/homeIndex${imgAffix}.jpg`} placement="left" alt="Screenshot: Upcoming Health Checks" loading="lazy" />
+        <ScreenshotTablet src={`/projects/carrier/gallery/${imgLocale}/furryShowOnTime${imgAffix}.jpg`} placement="right" alt="Screenshot: View the details of Seven" loading="lazy" />
       </Device>
     </>
   )
