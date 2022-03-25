6786c7014fdcee2a0f65d4c6d9f32f31563376a9
ymcheung <ymcheung.tw@me.com>
2022-03-25 11:26:25 +0800

    Update iterations in Pie

--- pages/pie-clockin.tsx
+++ pages/pie-clockin.tsx
@@ -36,7 +36,7 @@ export default function ProjectPie() {
     name: t('home:project.pie.title', { redesign: t('home:verb.redesign') }),
     description: t('home:project.pie.description'),
     datePublished: '2020-08-29',
-    dateModified: '2022-03-15'
+    dateModified: '2022-03-25'
   };
 
   useEffect(() => {
